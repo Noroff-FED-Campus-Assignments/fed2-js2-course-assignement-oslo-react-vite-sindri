ff7054ac835ae5433505d0f7d8df6b4ac9a4add6
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-10-12 10:55:29 +0200

    added code for fetching posts

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Route } from "@tanstack/react-router";
+import { API_URL } from "./../lib/constants";
 
 const initialPostState = {
   title: "No post found",
@@ -12,13 +14,26 @@ const initialPostState = {
  * @see https://docs.noroff.dev/social-endpoints/posts
  */
 export default function PostPage() {
-  const [post, setPost] = useState(initialPostState);
+  const [post, setPost] = useState([initialPostState]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // TIP: Get the ID from the search params in the URL
+        const postId = window.location.pathname.split("/")[2];
+        const token = localStorage.getItem("access_token");
         // TIP: Fetch the post from the API using the ID
+
+        fetch(`${API_URL}/posts/${postId}`, {
+          method: "GET",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODEsIm5hbWUiOiJmcm9kbG8iLCJlbWFpbCI6ImZpcnN0Lmxhc3RAc3R1ZC5ub3JvZmYubm8iLCJhdmF0YXIiOm51bGwsImJhbm5lciI6bnVsbCwiaWF0IjoxNjk2MzI0NjI2fQ.RzasPhTGOgkBdavgA1eObqzH5udnxJWvEksh5iEJ1zE",
+          },
+        })
+          .then((response) => response.json())
+          .then((post) => setPost(post));
+
         // TIP: Set the post in state
       } catch (error) {
         // TIP: Handle errors from the API
@@ -32,9 +47,10 @@ export default function PostPage() {
 
   return (
     <>
-      <h1>A single post</h1>
+      <h1>{post?.title}</h1>
       <section>
-        <h2>{post?.title}</h2>
+        <p>{post?.body}</p>
+        <img src={post?.media} alt="" />
       </section>
     </>
   );
