148ed7859203af0a0a5e4cae9cbd8e005ed6a9df
Hallvard-Benan <hallvard.benan@gmail.com>
2023-10-15 23:31:36 +0200

    feat: to top button on scroll

---
+++ src/components/to-top/index.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import "./toTop.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+export default function ToTopBtn() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 200 ? setIsVisible(true) : setIsVisible(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return isVisible ? (
+    <div className="to-top-container">
+      <button className="to-top visible" onClick={scrollToTop}>
+        to top <FontAwesomeIcon icon={faArrowUp} />
+      </button>
+    </div>
+  ) : null;
+}
